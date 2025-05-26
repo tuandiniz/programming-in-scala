358a6c09bf141da66b409e0d72c44c27f3367142
Tuan Diniz <tuandm@gmail.com>
2025-05-26 07:43:08 -0700

    Adding enum example

--- ch19/enums/src/main/scala/Main.scala
+++ ch19/enums/src/main/scala/Main.scala
@@ -5,3 +5,12 @@ import com.tuandiniz.Direction
 
   println(s"Direction $dir and It's inverse ${dir.invert}")
   println(s"All directions: ${Direction.values.mkString(", ")}")
+  println(s"Ordinals: ${Direction.values.map(v => v.ordinal).mkString(" ,")}")
+  println(s"Nearest to 67: ${Direction.nearestTo(67)}")
+  println(s"Nearest to 172: ${Direction.nearestTo(172)}")
+  println(s"All but nearest to 280: ${allButNearest(280)}")
+
+def allButNearest(degrees: Int): List[Direction] = {
+  val nearest = Direction.nearestTo(degrees)
+  Direction.values.filter(v => v != nearest).toList
+}
--- ch19/enums/src/main/scala/com/tuandiniz/Direction.scala
+++ ch19/enums/src/main/scala/com/tuandiniz/Direction.scala
@@ -12,5 +12,15 @@ enum Direction(val degrees: Int):
       case East => West
       case South => North
       case West => East
-      
-  
+
+object Direction:
+  def nearestTo(deg: Int): Direction =
+    val rem = deg % 360
+    val angle = if rem < 0 then rem + 360 else rem
+    val (ne, se, sw, nw) = (45, 135, 225, 315)
+
+    angle match
+      case a if a > nw && a <= ne => North
+      case a if a > ne && a <= se => East
+      case a if a > se && a <= sw => South
+      case a if a > sw && a <= nw => West
